51e6d73d49abdf75d41c20955ecd5215a68620dc
Charles Lachance <charlesfire2@hotmail.com>
2020-10-22 14:43:33 -0400

    CommandHandler V1

--- src/JardinCollectif/CommandHandler.java
+++ src/JardinCollectif/CommandHandler.java
@@ -1,32 +1,98 @@
 package JardinCollectif;
 
-import Integer;
-import String;
-
-import java.util.Date;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.Class;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public abstract class CommandHandler extends JardinCollectifCommandHandler {
-  /* {src_lang=Java}*/
+import JardinCollectif.annotations.Command;
 
+public abstract class CommandHandler {
+  private final HashMap<String, Method> dispatcher;
 
   protected CommandHandler() {
-    return null;
+    dispatcher = new HashMap<>();
+
+    for (Method method : this.getClass().getMethods()) {
+      Command command = method.getAnnotation(Command.class);
+      if (command != null) {
+        dispatcher.put(command.value(), method);
+      }
+    }
   }
 
-  protected static String readString(StringTokenizer tokenizer) {
-    return null;
+  private static String readString(StringTokenizer tokenizer) throws IFT287Exception {
+    if (tokenizer.hasMoreElements()) {
+      return tokenizer.nextToken();
+    } else {
+      throw new IFT287Exception("Mauvais nombre de paramètres.");
+    }
   }
 
-  protected static Integer readInt(StringTokenizer tokenizer) {
-    return null;
+  /**
+   * Lecture d'un int java de la transaction entree a l'ecran
+   */
+  private static int readInt(StringTokenizer tokenizer) throws IFT287Exception {
+    if (tokenizer.hasMoreElements()) {
+      String token = tokenizer.nextToken();
+      try {
+        return Integer.valueOf(token).intValue();
+      } catch (NumberFormatException e) {
+        throw new IFT287Exception("Nombre attendu a la place de \"" + token + "\"");
+      }
+    } else {
+      throw new IFT287Exception("Mauvais nombre de paramètres.");
+    }
   }
 
-  protected static Date readDate(StringTokenizer tokenizer) {
-    return null;
+  private static Date readDate(StringTokenizer tokenizer) throws IFT287Exception {
+    if (tokenizer.hasMoreElements()) {
+      String token = tokenizer.nextToken();
+      try {
+        return Date.valueOf(token);
+      } catch (IllegalArgumentException e) {
+        throw new IFT287Exception("Date dans un format invalide - \"" + token + "\"");
+      }
+    } else {
+      throw new IFT287Exception("Mauvais nombre de paramètres.");
+    }
   }
 
-  public void handleCommand(String command) {
-  }
+  public void handleCommand(String command) throws IFT287Exception {
+    StringTokenizer tokenizer = new StringTokenizer(command, " ");
+    if (tokenizer.hasMoreTokens()) {
+      String commandName = tokenizer.nextToken();
+      Method method = dispatcher.get(commandName);
 
+      if (method == null) {
+        throw new IFT287Exception(command + " : Transaction non reconnue.");
+      }
+
+      List<Object> params = new ArrayList<>();
+      Class[] paramTypes = method.getParameterTypes();
+      for (Class type : paramTypes) {
+        if (type.equals(Integer.class) || type.equals(int.class)) {
+          params.add(readInt(tokenizer));
+        } else if (type.equals(String.class)) {
+          params.add(readString(tokenizer));
+        } else if (type.equals(Date.class)) {
+          params.add(readDate(tokenizer));
+        } else {
+          throw new IFT287Exception("Unsupported parameter type in command " + method.getName() + ".");
+        }
+      }
+
+      try {
+        method.invoke(this, params.toArray());
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        System.out.println(e.getCause().getMessage());
+      }
+    }
+  }
 }
