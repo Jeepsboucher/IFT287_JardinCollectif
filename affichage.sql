98e8ab02a79eedf3ecc2f2beac1464aec9f5eca4
Charles Lachance <charlesfire2@hotmail.com>
2020-11-03 10:25:39 -0500

    Added objectdb and removed sql script

--- affichage.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM RequestToJoin;
-
-SELECT * FROM IsSowedIn;
-
-SELECT * FROM Member;
-
-SELECT * FROM Plant;
-
-SELECT * FROM Lot;
--- destruction.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IsSowedIn;
-DROP TABLE RequestToJoin;
-DROP TABLE Member;
-DROP TABLE Lot;
-DROP TABLE Plant;
